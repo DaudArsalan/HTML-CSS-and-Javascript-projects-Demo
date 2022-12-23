01308ff7007d66be6a1497f143f5128235bdb4b6
Daud <daudarslan869@gmail.com>
2022-12-24 01:44:10 +0500

    modify carditems.js

--- src/components/CardItems.js
+++ src/components/CardItems.js
@@ -6,8 +6,8 @@ export default function CardItems(props) {
   return (
     <div>
       <a href={link} target="_blank" rel="noreferrer">
-        <div className="max-w-sm bg-white shadow dark:bg-gray-800 dark:border-gray-700">
-          <img className="" src={image} alt="Project Preview" />
+        <div className="max-w-sm bg-white dark:bg-gray-800 dark:border-gray-700 transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110 duration-500 shadow-lg">
+          <img className="scale-100" src={image} alt="Project Preview" />
 
           <div className="py-3 px-5 bg-[#f8e881]">
             <h5 className="text-2xl text-[#6B5D00] text-center font-semibold tracking-widest dark:text-white">
